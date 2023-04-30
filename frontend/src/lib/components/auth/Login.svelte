62c328d8ea4ec20929bcc38fda2890e7fc9e97fe
Craig Post <sirgwain@gmail.com>
2023-04-30 11:20:35 -0700

    Made discord login the default

--- frontend/src/lib/components/auth/Login.svelte
+++ frontend/src/lib/components/auth/Login.svelte
@@ -25,33 +25,50 @@
 	let user = '';
 	let passwd = '';
 	$: loginError = '';
+	let showAdmin = false;
 </script>
 
 <div class="flex items-center justify-center min-h-screen card">
 	<div class="px-8 py-6 mt-4 bg-base-200 shadow-xl">
 		<h2 class="text-2xl card-title">Login</h2>
 		<div class="card-body">
-			<form on:submit|preventDefault={onSubmit}>
-				<label class="label block">
-					<div class="label-text">Username</div>
-					<input bind:value={user} required type="text" name="user" class="input input-bordered" />
-				</label>
+			<DiscordLink />
+			<button class="cs-link" on:click={() => (showAdmin = !showAdmin)}>
+				{#if showAdmin}
+					I'm not an admin
+				{:else}
+					I'm an admin
+				{/if}
+			</button>
+			{#if showAdmin}
+				<!-- content here -->
+				<form on:submit|preventDefault={onSubmit}>
+					<label class="label block">
+						<div class="label-text">Username</div>
+						<input
+							bind:value={user}
+							required
+							type="text"
+							name="user"
+							class="input input-bordered"
+						/>
+					</label>
 
-				<label class="label block">
-					<div class="label-text">Password</div>
-					<input
-						bind:value={passwd}
-						required
-						type="password"
-						name="passwd"
-						class="input input-bordered"
-					/>
-				</label>
-				<button class="btn btn-primary" type="submit">Submit</button>
-				<DiscordLink />
-			</form>
-			{#if loginError}
-				<div class="text-red-600">{loginError}</div>
+					<label class="label block">
+						<div class="label-text">Password</div>
+						<input
+							bind:value={passwd}
+							required
+							type="password"
+							name="passwd"
+							class="input input-bordered"
+						/>
+					</label>
+					<button class="btn btn-primary" type="submit">Submit</button>
+				</form>
+				{#if loginError}
+					<div class="text-red-600">{loginError}</div>
+				{/if}
 			{/if}
 		</div>
 	</div>
