f806f7dc7b64385b047c3fd86e2aee1192db2aef
sirgwain <sirgwain@gmail.com>
2023-06-18 18:04:13 -0700

    messing with ios fullscreen

--- frontend/src/routes/(user)/techs/[name]/+layout.svelte
+++ frontend/src/routes/(user)/techs/[name]/+layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import Menu from '$lib/components/Menu.svelte';
-	import { me } from '$lib/services/Context';
 </script>
 
-<div class="mx-auto md:max-w-2xl">
+<!-- add a margin on small screens so it will scroll -->
+<div class="mx-auto md:max-w-2xl mb-10 md:mb-0">
 	<slot>This is the main content</slot>
 </div>
