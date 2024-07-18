74da8dc79ab03d727c40e268344507a19b71c275
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2024-07-18 10:47:42 -0400

    Update LRTsDescriptions.svelte
    Slight additional typo fixes

--- frontend/src/lib/components/game/race/LRTsDescriptions.svelte
+++ frontend/src/lib/components/game/race/LRTsDescriptions.svelte
@@ -14,11 +14,11 @@
 			case LRT.ISB:
 				return 'Gives you two new starbase designs. The Space Dock hull is a cheap starbase with a small space dock that can refuel ships and build small to medium vessels. The Ultra-Station is much larger and better fortified than a standard Starbase. Your starbases are automatically cloaked by 20% and cost 20% less to build.';
 			case LRT.GR:
-				return 'Your race takes a holistic approach to research. Only half of the resources dedicated to research will be applied to the current field of research. 15% of the total will be applied to each of the fields.';
+				return 'Your race takes a holistic approach to research. Only half of the resources dedicated to research will be applied to the currently selected field, but 15% of the amount will be applied to all other fields (for a total of 125%).';
 			case LRT.UR:
-				return 'When you scrap a fleet at a starbase, you will recover 90% of the minerals and 70% of the resources used to produce the fleet, avaliable for production in the next year. Scrapping at a planet gives you half the amount of minerals and resources.';
+				return 'When you scrap a fleet at a starbase, you will recover 90% of the minerals and up to 70% of the resources used to produce the fleet, avaliable for use the following year. Scrapping at a planet will yield half the amount of minerals and resources.';
 			case LRT.NRSE:
-				return 'You will not be able to build the Radiating Hydro-Ram Scoop, Sub-Galactic Fuel Scoop, Trans-Galactic Fuel Scoop, Trans-Galactic Super Scoop, Trans-Galactic Mizer Scoop or the Galaxy Scoop. You will be able to build the Interspace-10 engine, which can travel at warp 10 without taking damage and has lower technology requirements.';
+				return 'You will not be able to build the Radiating Hydro-Ram Scoop, Sub-Galactic Fuel Scoop, Trans-Galactic Fuel Scoop, Trans-Galactic Super Scoop, Trans-Galactic Mizer Scoop or the Galaxy Scoop. You will be able to build the Interspace-10 engine, which can safely travel at warp 10 and has modest technology requirements.';
 			case LRT.OBRM:
 				return "The only mining ship available to you will be the Mini-Miner. This trait overrides 'Advanced Remote Mining'. Your maximum population per planet is increased by 10%.";
 			case LRT.NAS:
@@ -26,9 +26,9 @@
 			case LRT.LSP:
 				return 'Instead of 25000 people, you start with 17500 (30% fewer). It takes a long time to overcome a lower starting population - it helps to have a high growth rate, but even then it can be painful.';
 			case LRT.BET:
-				return 'New technologies initially cost twice as much to build. As soon as you exceed all of the tech requirements by one level, the cost drops back to normal. Miniaturization (the lowering of production costs for lower-tech items) occurs at 5% per level, up to 80%. (Without this trait, miniaturization occurs at 4% and caps at 75%.)';
+				return 'New technologies initially cost twice as much to build. As soon as you exceed all of the tech requirements by one level, the cost drops back to normal. Miniaturization (the lowering of production costs at higher tech levels) occurs at 5% per level, up to 80%. (Without this trait, miniaturization occurs at 4% per level and caps at 75%.)';
 			case LRT.RS:
-				return 'All shields are 40% stronger than their listed rating. Shields regenerate 10% of their maximum strength after each round of battle until destroyed. However, all armor components will only have 50% of their rated strength.';
+				return 'All shields are 40% stronger than their listed rating. Shields regenerate 10% of their maximum strength after each round of battle until destroyed. However, any armor components added to your ships will only have 50% of their rated strength.';
 			case LRT.MA:
 				return 'You will be able to turn resources into minerals more efficiently. One instance of mineral alchemy will use 25 resources (instead of 100) to produce 1kT of each mineral.';
 			case LRT.CE:
