899a75b6ac77a9cbdddbca8d41dd59d6a1e615a0
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2024-08-04 09:28:23 -0400

    Update intel.go
    Updated mineral packet function to use new function estimateDamage instead of getDamage

--- cs/intel.go
+++ cs/intel.go
@@ -560,7 +560,7 @@ func (d *discover) discoverMineralPacket(rules *Rules, mineralPacket *MineralPac
 
 	if player.Num != mineralPacket.PlayerNum {
 		if target.PlayerNum == player.Num {
-			damage := mineralPacket.getDamage(rules, target, player)
+			damage := mineralPacket.estimateDamage(rules, PacketPlayer, target, player)
 			messager.mineralPacketDiscoveredTargettingPlayer(player, mineralPacket, packetPlayer, target, damage)
 		} else {
 			messager.mineralPacketDiscovered(player, mineralPacket, packetPlayer, target)
