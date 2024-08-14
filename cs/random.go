51e42c991fec164254b1d794c9a8e047fffc0899
sirgwain <sirgwain@gmail.com>
2024-08-13 18:46:57 -0700

    added comments on random for intellisense

--- cs/random.go
+++ cs/random.go
@@ -2,8 +2,16 @@ package cs
 
 // the rng rules all
 type rng interface {
+	// Float64 returns, as a float64, a pseudo-random number in [0.0,1.0)
+	// from the default Source.
 	Float64() float64
+
+	// Intn returns, as an int, a non-negative pseudo-random number in [0,n).
+	// It panics if n <= 0.
 	Intn(n int) int
+
+	// Shuffle pseudo-randomizes the order of elements using the default Source.
+	// n is the number of elements. Shuffle panics if n < 0.
+	// swap swaps the elements with indexes i and j.
 	Shuffle(n int, swap func(i, j int))
 }
- 
