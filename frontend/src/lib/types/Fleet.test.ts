bd8206e89e0cf96f61db8dd48f65eec3df638673
sirgwain <sirgwain@gmail.com>
2024-09-11 08:00:04 -0700

    fixed unit tests

--- frontend/src/lib/types/Fleet.test.ts
+++ frontend/src/lib/types/Fleet.test.ts
@@ -13,59 +13,30 @@ describe('Fleet test', () => {
 	} as Fleet);
 	it('returns minimal speeds for distances', () => {
 		const player = new Player();
+		const origin = { position: { x: 0, y: 0 } } as MapObject;
 		// one year to go 49 ly
 		expect(
-			fleet.getMinimalWarp(
-				player,
-				{ position: {} } as MapObject,
-				{ position: { x: 49 } } as MapObject,
-				0,
-				7
-			)
+			fleet.getMinimalWarp(player, origin, { position: { x: 49, y: 0 } } as MapObject, 0, 7)
 		).toBe(7);
 
 		// two years at warp 7, two at warp 6 or warp 5, pick warp 5
 		expect(
-			fleet.getMinimalWarp(
-				player,
-				{ position: {} } as MapObject,
-				{ position: { x: 50 } } as MapObject,
-				0,
-				7
-			)
+			fleet.getMinimalWarp(player, origin, { position: { x: 50, y: 0 } } as MapObject, 0, 7)
 		).toBe(5);
 
 		// warp 6 takes 3 years to 72, so in 73 we can make it in 2 at warp 7
 		expect(
-			fleet.getMinimalWarp(
-				player,
-				{ position: {} } as MapObject,
-				{ position: { x: 73 } } as MapObject,
-				0,
-				7
-			)
+			fleet.getMinimalWarp(player, origin, { position: { x: 73, y: 0 } } as MapObject, 0, 7)
 		).toBe(7);
 
 		// // this is obvious
 		expect(
-			fleet.getMinimalWarp(
-				player,
-				{ position: {} } as MapObject,
-				{ position: { x: 36 } } as MapObject,
-				0,
-				7
-			)
+			fleet.getMinimalWarp(player, origin, { position: { x: 36, y: 0 } } as MapObject, 0, 7)
 		).toBe(6);
 
 		// // might as well go warp 5
 		expect(
-			fleet.getMinimalWarp(
-				player,
-				{ position: {} } as MapObject,
-				{ position: { x: 25 } } as MapObject,
-				0,
-				7
-			)
+			fleet.getMinimalWarp(player, origin, { position: { x: 25, y: 0 } } as MapObject, 0, 7)
 		).toBe(5);
 	});
 });
