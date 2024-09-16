aac8b6236613b0b41ff902085525b8fe7e76e7e3
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2024-09-16 10:24:25 -0400

    removed else if

--- cs/costcalculator.go
+++ cs/costcalculator.go
@@ -208,7 +208,8 @@ func (p *costCalculate) GetDesignCost(rules *Rules, techLevels TechLevel, raceSp
 	hull := rules.techs.GetHull(design.Hull)
 	if hull == nil {
 		return Cost{}, fmt.Errorf("hull design %s not found in tech store", design.Hull)
-	} else if hull.Starbase {
+	}
+	if hull.Starbase {
 		starbase = true
 	}
 
