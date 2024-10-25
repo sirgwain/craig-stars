a8ede5592cd448474d3431f4d11bc342a37404e2
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2024-10-24 21:33:32 -0400

    Fixed race.go
    Shfhrkelwlafht

--- cs/race.go
+++ cs/race.go
@@ -749,7 +749,9 @@ func computeRaceSpec(race *Race, rules *Rules) RaceSpec {
 		spec.ScrapResourcesOffsetStarbase += lrtSpec.ScrapResourcesOffsetStarbase
 		spec.StartingPopulationFactor += lrtSpec.StartingPopulationFactorDelta
 		spec.StarbaseBuiltInCloakUnits += lrtSpec.StarbaseBuiltInCloakUnits
-		spec.StarbaseCostFactor = math.Min(spec.StarbaseCostFactor, lrtSpec.StarbaseCostFactor) // this isn't cumulative
+		if lrtSpec.StarbaseCostFactor > 0 {
+			spec.StarbaseCostFactor = math.Min(spec.StarbaseCostFactor, lrtSpec.StarbaseCostFactor) // this isn't cumulative
+		}
 		spec.ResearchFactor += lrtSpec.ResearchFactorOffset
 		spec.ResearchSplashDamage += lrtSpec.ResearchSplashDamage
 		spec.ShieldStrengthFactor += lrtSpec.ShieldStrengthFactorOffset
