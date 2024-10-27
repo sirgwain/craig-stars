f80ca7368973f0eb60563612ec46c93a5c4808e5
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2024-10-26 20:08:03 -0400

    Fixed techstore.go organic cost (#560)
    * Fixed techstore.go organic cost
    
    * updated techs.json
    
    ---------
    
    Co-authored-by: sirgwain <sirgwain@gmail.com>

--- cs/techstore.go
+++ cs/techstore.go
@@ -1448,7 +1448,7 @@ var Crobmnium = TechHullComponent{Tech: NewTech("Crobmnium", NewCost(6, 0, 0, 13
 	Armor:        75,
 	HullSlotType: HullSlotTypeArmor,
 }
-var Carbonic = TechHullComponent{Tech: NewTech("Carbonic Armor", NewCost(5, 0, 0, 15), TechRequirements{TechLevel: TechLevel{Biotechnology: 4}}, 30, TechCategoryArmor),
+var Carbonic = TechHullComponent{Tech: NewTech("Carbonic Armor", NewCost(0, 0, 5, 15), TechRequirements{TechLevel: TechLevel{Biotechnology: 4}}, 30, TechCategoryArmor),
 
 	Mass:         25,
 	Armor:        100,
