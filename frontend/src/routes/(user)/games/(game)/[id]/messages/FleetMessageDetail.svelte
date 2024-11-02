6ee3e4688450f86cb66c56daab556e63fc449c71
sirgwain <sirgwain@gmail.com>
2024-11-02 09:20:45 -0700

    fixed double multiplier for radiating ram scoop

--- frontend/src/routes/(user)/games/(game)/[id]/messages/FleetMessageDetail.svelte
+++ frontend/src/routes/(user)/games/(game)/[id]/messages/FleetMessageDetail.svelte
@@ -131,14 +131,15 @@
 	Your patrolling {message.targetName} has targeted {message.spec.targetName} to intercept.
 {:else if message.type === MessageType.FleetRadiatingEngineDieoff}
 	<!-- Colonist dieoff from engine radiation -->
-	Engine radiation has killed {(message.spec.amount ?? 0) * 100} colonists traveling in {message.targetName}.
+	Engine radiation has killed {(message.spec.amount ?? 0).toLocaleString()} colonists traveling in {message.targetName}.
 {:else if message.type === MessageType.FleetReproduce}
 	{#if !message.spec.amount2 || !message.spec.targetNum}
 		Your colonists in {message.targetName} have made good use of their time increasing their on-board
 		number by {message.spec.amount} colonists.
 	{:else}
 		<!-- TODO: actually fix bug non jankily by multiplying message.amount2 by 100 during assignment-->
-		Breeding activities on {message.targetName} have overflowed living space. {message.spec.amount2 * 100}
+		Breeding activities on {message.targetName} have overflowed living space. {message.spec
+			.amount2 * 100}
 		colonists have been beamed down to {message.spec.targetName}.
 	{/if}
 	<!-- Remote Mining messages -->
@@ -148,14 +149,12 @@
 		boranium: message.spec.mineral?.boranium ?? 0,
 		germanium: message.spec.mineral?.germanium ?? 0
 	}}
-	{message.targetName} has remote mined {message.spec.targetName} extracting {andCommaList(
-		[
-			minerals.ironium > 0 ? `${minerals.ironium}kT of Ironium` : '',
-			minerals.boranium > 0 ? `${minerals.boranium}kT of Boranium` : '',
-			minerals.germanium > 0 ? `${minerals.germanium}kT of Germanium` : ''
-		] + '.',
-		'no minerals.'
-	)}
+	{message.targetName} has remote mined {message.spec.targetName} extracting {(andCommaList([
+		minerals.ironium > 0 ? `${minerals.ironium}kT of Ironium` : '',
+		minerals.boranium > 0 ? `${minerals.boranium}kT of Boranium` : '',
+		minerals.germanium > 0 ? `${minerals.germanium}kT of Germanium` : ''
+	]) + '.',
+	'no minerals.')}
 {:else if message.type === MessageType.FleetScrapped}
 	{message.targetName} has been dismantled. The scrap was left in deep space.
 {:else if message.type === MessageType.FleetTransferGiven}
