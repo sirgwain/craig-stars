bcce455087e8b7a71624a30e5cdf3a4cf6caeac3
sirgwain <sirgwain@gmail.com>
2024-12-08 11:27:33 -0700

    added back in mistakenly deleted select component

---
+++ frontend/src/lib/components/Select.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { startCase } from 'lodash-es';
+	import type { HTMLSelectAttributes } from 'svelte/elements';
+
+	type T = $$Generic;
+
+	type Value<T> = {
+		value: T;
+		title: string;
+	};
+
+	type Props = {
+		name: string;
+		title?: string | undefined;
+		tooltip?: string | undefined;
+		titleClass?: string;
+		values?: Value<T>[];
+	} & HTMLSelectAttributes;
+
+	let {
+		name,
+		value = $bindable(),
+		title = startCase(name),
+		titleClass = 'label-text w-32 text-right',
+		values = [],
+		...rest
+	}: Props = $props();
+</script>
+
+<div class="w-full flex-grow">
+	<label class="label"
+		><span class={titleClass}>{title}</span>
+		<select class="select input-bordered ml-2 flex-grow" bind:value {...rest}>
+			{#each values as value}
+				<option value={value.value}>{value.title}</option>
+			{/each}
+		</select>
+	</label>
+</div>
