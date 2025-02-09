25d1f026f7cf334494fa256eab01be0137695782
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-02-09 15:54:59 -0500

    Fixed Genesis Device resetting production queue (#602)
    Also added planet hab randomization rules

--- cs/planet_test.go
+++ cs/planet_test.go
@@ -275,53 +275,69 @@ func Test_computePlanetSpec(t *testing.T) {
 }
 
 func TestPlanet_randomize(t *testing.T) {
-
-	type args struct {
-		rng rng
+	type fields struct {
+		habDropoff Hab
+		minHab     int
+		maxHab     int
 	}
 	tests := []struct {
-		name string
-		args args
-		want Planet
+		name   string
+		fields fields
+		planet *Planet
+		rng    rng
+		want   *Planet
 	}{
 		{
-			name: "planet gen with all 0 rng",
-			args: args{newIntRandom()},
-			want: Planet{
+			name:   "normal w/ all 0 rng; shouldn't reset production queue",
+			fields: fields{rules.HabDropoffRange, rules.MinHab, rules.MaxHab},
+			planet: NewPlanet().WithOrders(PlanetOrders{
+				ProductionQueue: []ProductionQueueItem{
+					{Type: QueueItemTypeFactory, Quantity: 1, Allocated: Cost{0, 0, 2, 6}},
+					{Type: QueueItemTypeAutoDefenses, Quantity: 100},
+					{Type: QueueItemTypeAutoFactories, Quantity: 10},
+				},
+			}),
+			rng: newIntRandom(),
+			want: &Planet{
 				MapObject:            MapObject{Type: MapObjectTypePlanet, PlayerNum: Unowned},
 				Dirty:                true,
 				Hab:                  Hab{1, 1, 1},
 				BaseHab:              Hab{1, 1, 1},
 				MineralConcentration: Mineral{1, 1, 1},
-				PlanetOrders: PlanetOrders{
-					ProductionQueue: []ProductionQueueItem{},
-				},
+				MineYears:            Mineral{},
+				PlanetOrders: PlanetOrders{ProductionQueue: []ProductionQueueItem{
+					{Type: QueueItemTypeFactory, Quantity: 1, Allocated: Cost{0, 0, 2, 6}},
+					{Type: QueueItemTypeAutoDefenses, Quantity: 100},
+					{Type: QueueItemTypeAutoFactories, Quantity: 10},
+				}},
 			},
 		},
 		{
-			name: "planet gen random minerals",
-			args: args{newIntRandom()},
-			want: Planet{
+			name:   "custom rules/rng seed",
+			fields: fields{Hab{20, 20, 20}, 10, 90}, // 10 + rand[0,61) + rand[0,21)
+			planet: NewPlanet(),
+			rng:    newIntRandom(50, 43, 11, 2, 3, 5),
+			want: &Planet{
 				MapObject:            MapObject{Type: MapObjectTypePlanet, PlayerNum: Unowned},
 				Dirty:                true,
-				Hab:                  Hab{1, 1, 1},
-				BaseHab:              Hab{1, 1, 1},
+				Hab:                  Hab{62, 56, 26},
+				BaseHab:              Hab{62, 56, 26},
 				MineralConcentration: Mineral{1, 1, 1},
-				PlanetOrders: PlanetOrders{
-					ProductionQueue: []ProductionQueueItem{},
-				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewPlanet()
+			got := tt.planet
+			r := &rules
+			r.HabDropoffRange = tt.fields.habDropoff
+			r.MinHab = tt.fields.minHab
+			r.MaxHab = tt.fields.maxHab
+			r.random = tt.rng
 
-			rules := NewRules()
-			rules.random = tt.args.rng
-			got.randomize(&rules)
+			got.randomize(r)
 
-			if !reflect.DeepEqual(got, &tt.want) {
+			if !reflect.DeepEqual(got, tt.want) {
 				// dump json, but this won't include some fields
 				test.CompareAsJSON(t, got, tt.want)
 				t.Errorf("randomize() = %#v, want %#v", got, tt.want)
