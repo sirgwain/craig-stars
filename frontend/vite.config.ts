adcac34dd6a6c37de0e3a2f3354bd585af6abca2
sirgwain <sirgwain@gmail.com>
2025-02-09 19:45:03 -0700

    removed tabwidth change for prettier, ran format to have a baseline for any future formatting changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ const pkg = JSON.parse(json);
 
 export default defineConfig(({ mode }) => ({
 	test: {
-		reporters: process.env.CI ? ["junit", "github-actions"] : "default",
+		reporters: process.env.CI ? ['junit', 'github-actions'] : 'default',
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		environment: 'jsdom',
 		setupFiles: ['e2e/setup.ts']
