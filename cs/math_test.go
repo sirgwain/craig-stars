678f3ac52e457b2bc86bfd13a2a1126b04719e85
sirgwain <sirgwain@gmail.com>
2025-03-01 16:27:27 -0700

    added utility function to handle splitting cargo or fuel.

--- cs/math_test.go
+++ cs/math_test.go
@@ -1,6 +1,9 @@
 package cs
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestClamp(t *testing.T) {
 	type args struct {
@@ -90,6 +93,105 @@ func TestPowInt(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := PowInt(tt.base, tt.exponent); got != tt.want {
 				t.Errorf("PowInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_splitValues(t *testing.T) {
+	type args struct {
+		sourceCapacity int
+		destCapacity1  int
+		destCapacity2  int
+		values         []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want1   []int
+		want2   []int
+		wantErr bool
+	}{
+		{
+			name:    "invalid args",
+			args:    args{sourceCapacity: 4, destCapacity1: 5, destCapacity2: 0},
+			wantErr: true,
+		},
+		{
+			name:  "no split",
+			args:  args{sourceCapacity: 4, destCapacity1: 4, destCapacity2: 0, values: []int{1, 1, 1, 1}},
+			want1: []int{1, 1, 1, 1},
+			want2: []int{0, 0, 0, 0},
+		},
+		{
+			name: "split all",
+
+			args:  args{sourceCapacity: 4, destCapacity1: 0, destCapacity2: 4, values: []int{1, 1, 1, 1}},
+			want1: []int{0, 0, 0, 0},
+			want2: []int{1, 1, 1, 1},
+		},
+		{
+			name: "split even",
+
+			args:  args{sourceCapacity: 8, destCapacity1: 4, destCapacity2: 4, values: []int{2, 2, 2, 2}},
+			want1: []int{1, 1, 1, 1},
+			want2: []int{1, 1, 1, 1},
+		},
+		{
+			name: "split 1",
+
+			args:  args{sourceCapacity: 8, destCapacity1: 7, destCapacity2: 1, values: []int{2, 2, 2, 2}},
+			want1: []int{2, 2, 2, 1},
+			want2: []int{0, 0, 0, 1},
+		},
+		{
+			name: "split 2",
+
+			args:  args{sourceCapacity: 8, destCapacity1: 6, destCapacity2: 2, values: []int{2, 2, 2, 2}},
+			want1: []int{2, 2, 2, 0},
+			want2: []int{0, 0, 0, 2},
+		},
+		{
+			name: "split 3",
+
+			args:  args{sourceCapacity: 8, destCapacity1: 5, destCapacity2: 3, values: []int{2, 2, 2, 2}},
+			want1: []int{1, 1, 1, 2},
+			want2: []int{1, 1, 1, 0},
+		},
+		{
+			name: "split 2/3",
+
+			args:  args{sourceCapacity: 10, destCapacity1: 7, destCapacity2: 3, values: []int{3, 3, 3, 1}},
+			want1: []int{2, 2, 2, 1},
+			want2: []int{1, 1, 1, 0},
+		},
+		{
+			name: "split many",
+
+			args:  args{sourceCapacity: 2000, destCapacity1: 1500, destCapacity2: 500, values: []int{100, 200, 300, 400}},
+			want1: []int{75, 150, 225, 300},
+			want2: []int{25, 50, 75, 100},
+		},
+		{
+			name: "split 33%",
+
+			args:  args{sourceCapacity: 300, destCapacity1: 200, destCapacity2: 100, values: []int{100, 0, 0, 0}},
+			want1: []int{67, 0, 0, 0},
+			want2: []int{33, 0, 0, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got1, got2, err := splitValues(tt.args.sourceCapacity, tt.args.destCapacity1, tt.args.destCapacity2, tt.args.values...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("splitValues() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("splitValues() got1 = %v, want %v", got1, tt.want1)
+			}
+			if !reflect.DeepEqual(got2, tt.want2) {
+				t.Errorf("splitValues() got2 = %v, want %v", got2, tt.want2)
 			}
 		})
 	}
