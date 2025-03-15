edb72671ffc12bd0f5ecef58eeab9bc930c90e75
Bertie690 <136088738+Bertie690@users.noreply.github.com>
2025-03-14 23:23:07 -0400

    Remove comment

--- cs/bomb.go
+++ cs/bomb.go
@@ -32,7 +32,7 @@ import (
 type Bomb struct {
 	Quantity             int     `json:"quantity,omitempty"`
 	KillRate             float64 `json:"killRate,omitempty"`
-	MinKillRate          int     `json:"minKillRate,omitempty"` //? Why is MinKillRate an int but KillRate a float?
+	MinKillRate          int     `json:"minKillRate,omitempty"`
 	StructureDestroyRate float64 `json:"structureDestroyRate,omitempty"`
 	UnterraformRate      int     `json:"unterraformRate,omitempty"`
 }
