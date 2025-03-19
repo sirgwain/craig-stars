d9ff7dabcb92e9f7768976d309d6e807eaca3c18
Bertie690 <taylormw163@gmail.com>
2025-03-18 21:22:50 -0400

    andCommandlist fixes

--- frontend/src/lib/andCommandList.test.ts
+++ frontend/src/lib/andCommandList.test.ts
@@ -13,4 +13,10 @@ describe('andCommandList', () => {
 		expect(andCommaList(['', 'apple yay'])).toBe('apple yay');
 		expect(andCommaList(['aeeee', '', 'apple yay'])).toBe('aeeee and apple yay');
 	});
+	it('supports custom operators', () => {
+		expect(andCommaList([], 'this is an empty string')).toBe('this is an empty string');
+		expect(andCommaList(['granny smith', 'honeycrisp', 'fuji', 'gala', 'pink lady'], 'or'))
+			.toBe('granny smith, honeycrisp, fuji, gala or pink lady');
+		expect(andCommaList(['aeeee', '', 'apple yay'], '', 'and/or maaaaybe')).toBe('aeeee and/or maaaaybe apple yay');
+	});
 });
--- frontend/src/lib/andCommandList.ts
+++ frontend/src/lib/andCommandList.ts
@@ -1,11 +1,12 @@
 /**
- * Concaentate several strings with commas, adding an "and" on the final word.
+ * Concaentate several strings with commas, adding a conjunction between the final 2 entries.
  * @param items a list of strings to concatenate
- * @param emptyResult a result to return if the string is empty
- * @returns The concaetation of all items
-*/
-export function andCommaList(items: string[], emptyResult = ''): string {
-	const filteredItems = items.filter((i) => !!i);
+ * @param emptyResult a result to return if the string is empty, default nothing
+ * @param finalConjunction A conjunction to use on the final word; defaults to the word 'and'.
+ * @returns A string containing all of items' elements delimited by commas and the chosen conjunction.
+ */
+export function andCommaList(items: string[], emptyResult = '', finalConjunction = 'and'): string {
+	const filteredItems = items.filter((i) => !!i); // remove empty strings
 
 	if (filteredItems.length == 0) {
 		return emptyResult;
@@ -16,17 +17,20 @@ export function andCommaList(items: string[], emptyResult = ''): string {
 	}
 
 	if (filteredItems.length == 2) {
-		return `${filteredItems[0]} and ${filteredItems[1]}`;
+		return `${filteredItems[0]} ${finalConjunction} ${filteredItems[1]}`;
 	}
 
-	return filteredItems.reduce((prevVal, currVal: string, i: number) => {
+	let result = filteredItems[0];
+
+	for (let i = 1; i < filteredItems.length; i++) {
 		if (i == filteredItems.length - 1) {
-			prevVal += ' and ';
-		} else if (i > 0) {
-			prevVal += ', ';
+			result += ` ${finalConjunction} `;
+		} else {
+			result += ', ';
 		}
 
-		return prevVal + currVal;
-	});
+		result += filteredItems[i];
+	}
 
+	return result;
 }
